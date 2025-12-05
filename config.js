586467a4e26c8f90804679aabc3261f129c043db
iagocamuzzi <iagocamuzzi2@gmail.com>
2025-12-05 12:41:52 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
+// config.js
 window.PELADA_CFG = {
   MERCADO_PAGO_LINK: "https://www.mercadopago.com.br/checkout/v1/redirect?pref_id=71896558-d60bbf91-0a06-4a04-bdd9-383868794d94",
 
   GOOGLE_FORM_ACTION: "https://docs.google.com/forms/d/e/1FAIpQLSeZmbDF0ZBwKS1sjG4h7232XjFjHKe6-jOm26E7qlTp40mnTg/formResponse",
+
   GOOGLE_FORM_ENTRY_NAME: "entry.422925853",
 
-  // 🔥 NOVA URL (INSTANTÂNEA, SEM CACHE)
-  SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/19lll3AIwKKhuH8LpSorboRxda--M02CLedbf277U-N8/gviz/tq?tqx=out:csv&gid=1883023472",
+  SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT38Fu2jVJcsnZ2xAIqJyw5vr-qLwHxJyvoY_gbe5YKNxLmv70G2KvTCxAEq6dFBXzmfGv-5hMY6OrI/pub?gid=1883023472&single=true&output=csv",
 
   TITULARES_MAX: 15,
   PRECO: 20,
