65e0325b35e2f6c1e4847f96e5ecade811dbcd0e
iagocamuzzi <iagocamuzzi2@gmail.com>
2025-12-05 13:53:56 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,19 @@
 // config.js
 window.PELADA_CFG = {
-  MERCADO_PAGO_LINK: "https://www.mercadopago.com.br/checkout/v1/redirect?pref_id=71896558-d60bbf91-0a06-4a04-bdd9-383868794d94",
+  FIREBASE: {
+    apiKey: "AIzaSyBHkn2f20TbpaZ0oU4nKt91O-4hxl8M780",
+    authDomain: "pelad-segunda.firebaseapp.com",
+    projectId: "pelad-segunda",
+    storageBucket: "pelad-segunda.firebasestorage.app",
+    messagingSenderId: "346189760762",
+    appId: "1:346189760762:web:f52ca3de59f5d4c750e143"
+  },
 
-  GOOGLE_FORM_ACTION: "https://docs.google.com/forms/d/e/1FAIpQLSeZmbDF0ZBwKS1sjG4h7232XjFjHKe6-jOm26E7qlTp40mnTg/formResponse",
-
-  GOOGLE_FORM_ENTRY_NAME: "entry.422925853",
-
-  SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT38Fu2jVJcsnZ2xAIqJyw5vr-qLwHxJyvoY_gbe5YKNxLmv70G2KvTCxAEq6dFBXzmfGv-5hMY6OrI/pub?gid=1883023472&single=true&output=csv",
-
+  COLLECTION: "inscricoes",
   TITULARES_MAX: 15,
   PRECO: 20,
-  POST_PAGAMENTO_REDIRECT: "inserir.html"
+
+  // Modo admin (REMOVER nomes no ADM)
+  // deixe FALSE por enquanto até ajustar regras
+  ADMIN_MODE: false
 };
